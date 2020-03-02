9507ddc097b7100aa3f8af2595e57d549acf38aa
Mark Swaffer <mdswaffer@gmail.com>
2020-03-02 15:18:09 +0000

    Feat: Graceful shutdown for pg boss and pg pool
    WATER-2565
    
    Updates the shut down of the Hapi server to include pg-boss and the pg
    connection pool.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ moment.locale('en-gb');
 // -------------- Require project code -----------------
 const config = require('./config');
 const routes = require('./src/routes.js');
+const db = require('./src/lib/connectors/db');
 
 // Initialise logger
 const { logger } = require('./src/logger');
@@ -82,9 +83,21 @@ process
   .on('unhandledRejection', processError('unhandledRejection'))
   .on('uncaughtException', processError('uncaughtException'))
   .on('SIGINT', async () => {
-    logger.info('stopping permit repo');
+    logger.info('Stopping import service');
+
     await server.stop();
-    return process.exit(0);
+    logger.info('1/3: Hapi server stopped');
+
+    await server.messageQueue.stop();
+    logger.info('2/3: Message queue stopped');
+    logger.info('Waiting 5 secs to allow pg-boss to finish');
+
+    setTimeout(async () => {
+      await db.pool.end();
+      logger.info('3/3: Connection pool closed');
+
+      return process.exit(0);
+    }, 5000);
   });
 
 start();
