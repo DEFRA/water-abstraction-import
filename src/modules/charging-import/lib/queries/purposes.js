e513de41aada90c382a08fc7eb1225d33277498f
Mark Swaffer <mdswaffer@gmail.com>
2020-06-17 11:10:08 +0100

    Feat: Adds loss factor to purpose use
    WATER-2731
    
    Updates the import of purpose_uses
    
    Sets the is_two_part_tariff flag to true if the code is 400, 420, 600 or
    620.

--- src/modules/charging-import/lib/queries/purposes.js
+++ src/modules/charging-import/lib/queries/purposes.js
@@ -1,37 +1,60 @@
+'use strict';
+
 const importPrimaryPurposes = `
-INSERT INTO water.purposes_primary (id, description, date_created, date_updated)
-SELECT p."CODE", p."DESCR", now(), now()
-FROM import."NALD_PURP_PRIMS" p
-ON CONFLICT (id)
-DO
-  UPDATE
-  SET
+  insert into water.purposes_primary (legacy_id, description, date_created, date_updated)
+  select p."CODE", p."DESCR", now(), now()
+  from import."NALD_PURP_PRIMS" p
+  on conflict (legacy_id)
+  do update
+  set
     description= excluded.description,
     date_updated = now();
 `;
 
 const importSecondaryPurposes = `
-INSERT INTO water.purposes_secondary (id, description, date_created, date_updated)
-SELECT p."CODE", p."DESCR", now(), now()
-FROM import."NALD_PURP_SECS" p
-ON CONFLICT (id)
-DO
-  UPDATE
-  SET
+  insert into water.purposes_secondary (legacy_id, description, date_created, date_updated)
+  select p."CODE", p."DESCR", now(), now()
+  from import."NALD_PURP_SECS" p
+  on conflict (legacy_id)
+  do update
+  set
     description= excluded.description,
     date_updated = now();
 `;
 
 const importUses = `
-INSERT INTO water.purposes_uses (id, description, date_created, date_updated)
-SELECT p."CODE", p."DESCR", now(), now()
-FROM import."NALD_PURP_USES" p
-ON CONFLICT (id)
-DO
-  UPDATE
-  SET
+  insert into water.purposes_uses (
+    legacy_id,
+    description,
+    date_created,
+    date_updated,
+    loss_factor,
+    is_two_part_tariff
+  )
+  select
+    p."CODE",
+    p."DESCR",
+    now(),
+    now(),
+    case
+      when p."ALSF_CODE" = 'V' then 'very low'::water.charge_element_loss
+      when p."ALSF_CODE" = 'L' then 'low'::water.charge_element_loss
+      when p."ALSF_CODE" = 'M' then 'medium'::water.charge_element_loss
+      when p."ALSF_CODE" = 'H' then 'high'::water.charge_element_loss
+      when p."ALSF_CODE" = 'N' then 'non-chargeable'::water.charge_element_loss
+    end,
+    case
+      when p."CODE" in ('400', '420', '600', '620') then true
+      else false
+    end
+  from import."NALD_PURP_USES" p
+  on conflict (legacy_id)
+  do update
+  set
     description= excluded.description,
-    date_updated = now();
+    date_updated = now(),
+    loss_factor = excluded.loss_factor,
+    is_two_part_tariff = excluded.is_two_part_tariff;
 `;
 
 exports.importPrimaryPurposes = importPrimaryPurposes;
