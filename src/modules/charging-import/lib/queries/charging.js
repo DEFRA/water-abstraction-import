e8842031a19fd570adceec582244945d758151a2
Tim <timtamimi@ip-192-168-68-103.eu-west-2.compute.internal>
2020-08-27 09:04:14 +0100

    Update charging.js

--- src/modules/charging-import/lib/queries/charging.js
+++ src/modules/charging-import/lib/queries/charging.js
@@ -106,74 +106,45 @@ INSERT INTO water.charge_elements
 (charge_element_id, charge_version_id, external_id, abstraction_period_start_day, abstraction_period_start_month,
 abstraction_period_end_day, abstraction_period_end_month, authorised_annual_quantity, season, season_derived,
 source, loss, purpose_primary_id, purpose_secondary_id, purpose_use_id, factors_overridden, billable_annual_quantity,
-time_limited_start_date, time_limited_end_date, description, date_created, date_updated)
-
-SELECT ie.charge_element_id, iv.charge_version_id, e."ID"::integer AS external_id,
+time_limited_start_date, time_limited_end_date, description, date_created, date_updated) SELECT ie.charge_element_id, iv.charge_version_id, e."ID"::integer AS external_id,
 e."ABS_PERIOD_ST_DAY"::integer AS abstraction_period_start_day,
 e."ABS_PERIOD_ST_MONTH"::integer AS abstraction_period_start_month,
 e."ABS_PERIOD_END_DAY"::integer AS abstraction_period_end_day,
 e."ABS_PERIOD_END_MONTH"::integer AS abstraction_period_end_month,
-e."AUTH_ANN_QTY"::numeric AS authorised_annual_quantity,
-
-(CASE e."ASFT_CODE"
+e."AUTH_ANN_QTY"::numeric AS authorised_annual_quantity, (CASE e."ASFT_CODE"
   WHEN 'S' THEN 'summer'
   WHEN 'W' THEN 'winter'
   WHEN 'A' THEN 'all year'
-END)::water.charge_element_season AS season,
-
-(CASE e."ASFT_CODE_DERIVED"
+END)::water.charge_element_season AS season, (CASE e."ASFT_CODE_DERIVED"
   WHEN 'S' THEN 'summer'
   WHEN 'W' THEN 'winter'
   WHEN 'A' THEN 'all year'
-END)::water.charge_element_season AS season_derived,
-
-(CASE e."ASRF_CODE"
+END)::water.charge_element_season AS season_derived, (CASE e."ASRF_CODE"
   WHEN 'S' THEN 'supported'
   WHEN 'U' THEN 'unsupported'
   WHEN 'K' THEN 'kielder'
   WHEN 'T' THEN 'tidal'
-END)::water.charge_element_source AS source,
-
-(CASE e."ALSF_CODE"
+END)::water.charge_element_source AS source, (CASE e."ALSF_CODE"
   WHEN 'H' THEN 'high'
   WHEN 'M' THEN 'medium'
   WHEN 'L' THEN 'low'
   WHEN 'V' THEN 'very low'
   WHEN 'N' THEN 'non-chargeable'
-END)::water.charge_element_loss AS loss,
-
-pp.purpose_primary_id,
+END)::water.charge_element_loss AS loss, pp.purpose_primary_id,
 ps.purpose_secondary_id,
-pu.purpose_use_id,
-
-e."FCTS_OVERRIDDEN"::boolean AS factors_overridden,
-
-NULLIF(e."BILLABLE_ANN_QTY", 'null')::numeric AS billable_annual_quantity,
-
-  case e."TIMELTD_ST_DATE"
+pu.purpose_use_id, e."FCTS_OVERRIDDEN"::boolean AS factors_overridden, NULLIF(e."BILLABLE_ANN_QTY", 'null')::numeric AS billable_annual_quantity, case e."TIMELTD_ST_DATE"
     when 'null' then null
     else to_date(e."TIMELTD_ST_DATE", 'DD/MM/YYYY')
-  end AS time_limited_start_date,
-
-  case e."TIMELTD_END_DATE"
+  end AS time_limited_start_date, case e."TIMELTD_END_DATE"
     when 'null' then null
     else to_date(e."TIMELTD_END_DATE", 'DD/MM/YYYY')
-  end AS time_limited_end_date,
-
-  NULLIF(e."DESCR", 'null') AS description,
-
-  NOW() AS date_created,
-  NOW() AS date_updated
-
-FROM import."NALD_CHG_ELEMENTS" e
+  end AS time_limited_end_date, NULLIF(e."DESCR", 'null') AS description, NOW() AS date_created,  NOW() AS date_updated FROM import."NALD_CHG_ELEMENTS" e
 JOIN water_import.charge_elements ie ON e."ID"::integer=ie.element_id AND e."ACVR_AABL_ID"::integer = ie.licence_id AND e."FGAC_REGION_CODE"::integer = ie.region_code
 JOIN water_import.charge_versions iv ON e."ACVR_AABL_ID"::integer=iv.licence_id AND e."ACVR_VERS_NO"::integer=iv.version AND e."FGAC_REGION_CODE"::integer = iv.region_code
 JOIN water.charge_versions v on iv.charge_version_id=v.charge_version_id 
 JOIN water.purposes_primary pp on e."APUR_APPR_CODE"=pp.legacy_id
 JOIN water.purposes_secondary ps on e."APUR_APSE_CODE"=ps.legacy_id
-JOIN water.purposes_uses pu on e."APUR_APUS_CODE"=pu.legacy_id
-
-ON CONFLICT (charge_element_id) DO UPDATE SET
+JOIN water.purposes_uses pu on e."APUR_APUS_CODE"=pu.legacy_id ON CONFLICT (charge_element_id) DO UPDATE SET
 charge_version_id=EXCLUDED.charge_version_id, 
 external_id=EXCLUDED.external_id,
 abstraction_period_start_day=EXCLUDED.abstraction_period_start_day,
@@ -191,7 +162,8 @@ billable_annual_quantity=EXCLUDED.billable_annual_quantity,
 time_limited_start_date=EXCLUDED.time_limited_start_date,
 time_limited_end_date=EXCLUDED.time_limited_end_date,
 description=EXCLUDED.description, 
-date_updated=EXCLUDED.date_updated;`;
+date_updated=EXCLUDED.date_updated;
+;`;
 
 // Deletes charge elements that are no longer present in the NALD import
 const cleanupChargeElements = `
