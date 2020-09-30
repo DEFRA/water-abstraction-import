5655a40e6fd80c1a2bb98ffe08ee28533fab7e55
Tim <timtamimi@ip-192-168-68-103.eu-west-2.compute.internal>
2020-09-30 16:49:35 +0100

    Revert "Adds UNIQUE constraint on ALTER TABLE water.financial_agreement_types"
    This reverts commit 8bdc843d98ecf5118e3c7004079ce598a2f7a442.

--- migrations/sqls/20200930153304-set-unique-constraint-on-financial-agreement-types-table-down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE water.financial_agreement_types
-DROP CONSTRAINT code_is_unique;
--- migrations/sqls/20200930153304-set-unique-constraint-on-financial-agreement-types-table-up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE water.financial_agreement_types
-ADD CONSTRAINT code_is_unique UNIQUE (financial_agreement_code);
