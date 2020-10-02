e4b8643190c7fa12fdf98f5372221ab77de6ec5f
Tim <timtamimi@ip-192-168-68-103.eu-west-2.compute.internal>
2020-10-02 10:29:19 +0100

    Update import-companies.js

--- src/modules/licence-import/connectors/queries/import-companies.js
+++ src/modules/licence-import/connectors/queries/import-companies.js
@@ -5,6 +5,6 @@ exports.initialise = `INSERT INTO water_import.import_companies
 SELECT p."FGAC_REGION_CODE"::integer, p."ID"::integer, NOW(), NOW()
 FROM import."NALD_PARTIES" p ON CONFLICT (region_code, party_id) DO NOTHING RETURNING *;`;
 
-exports.setImportedStatus = `UPDATE water_import.import_companies  SET imported=true, date_updated=NOW() WHERE region_code=$1 AND party_id=$2;`;
+exports.setImportedStatus = 'UPDATE water_import.import_companies  SET imported=true, date_updated=NOW() WHERE region_code=$1 AND party_id=$2;';
 
-exports.getPendingCount = `SELECT COUNT(*) FROM water_import.import_companies WHERE imported=false;`;
+exports.getPendingCount = 'SELECT COUNT(*) FROM water_import.import_companies WHERE imported=false;';
