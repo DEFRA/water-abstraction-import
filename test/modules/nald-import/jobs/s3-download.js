a64a54ea34ebfb92c93070ec56236bc8e387513a
James Carmichael <james@siteclick.co.uk>
2020-11-11 11:53:20 +0000

    Force NALD import to complete in test env regardless of etag value

--- test/modules/nald-import/jobs/s3-download.js
+++ test/modules/nald-import/jobs/s3-download.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { afterEach, beforeEach, experiment, test } = exports.lab = require('@hapi/lab').script();
 const { expect } = require('@hapi/code');
 const sandbox = require('sinon').createSandbox();
@@ -8,6 +10,7 @@ const applicationStateService = require('../../../../src/modules/nald-import/ser
 const s3Service = require('../../../../src/modules/nald-import/services/s3-service.js');
 
 const s3Download = require('../../../../src/modules/nald-import/jobs/s3-download');
+const config = require('../../../../config');
 
 const testDownloadOccurs = () => {
   test('a message is logged', async () => {
@@ -39,6 +42,8 @@ experiment('modules/nald-import/jobs/s3-download', () => {
     sandbox.stub(applicationStateService, 'save');
 
     sandbox.stub(s3Service, 'getEtag').resolves('test-etag');
+
+    sandbox.stub(config.import.nald, 'isEtagCheckEnabled').value(true);
   });
 
   afterEach(async () => {
@@ -144,6 +149,25 @@ experiment('modules/nald-import/jobs/s3-download', () => {
           etag: 'test-etag',
           state: { etag: 'test-etag', isDownloaded: true },
           isRequired: false
+        });
+      });
+    });
+
+    experiment('when the config disables etag check and the etag has not changed', () => {
+      beforeEach(async () => {
+        sandbox.stub(config.import.nald, 'isEtagCheckEnabled').value(false);
+        applicationStateService.get.resolves({
+          etag: 'test-etag',
+          isDownloaded: true
+        });
+        result = await s3Download.handler(job);
+      });
+
+      test('the handler resolves with the expected values', async () => {
+        expect(result).to.equal({
+          etag: 'test-etag',
+          state: { etag: 'test-etag', isDownloaded: true },
+          isRequired: true
         });
       });
     });
