893e1423fa2b025f5ce925e1b267916e0169c50f
Tim <tim@tamimi.dev>
2020-11-20 14:19:18 +0000

    Update 20201120135927-create-charge-versions-metadata-table-up.sql

--- migrations/sqls/20201120135927-create-charge-versions-metadata-table-up.sql
+++ migrations/sqls/20201120135927-create-charge-versions-metadata-table-up.sql
@@ -3,8 +3,10 @@ CREATE TYPE charge_version_metadata_status AS ENUM('current', 'superseded');
 CREATE TABLE water_import.charge_versions_metadata
 (
 	external_id varchar not null,
-	start_date timestamp not null,
-	end_date timestamp,
+	start_date date not null,
+	end_date date,
 	status charge_version_metadata_status not null,
-	is_nald_gap boolean not null
+	is_nald_gap boolean not null,
+	date_created timestamp with time zone NOT NULL DEFAULT NOW(),
+    date_updated timestamp with time zone,
 );
