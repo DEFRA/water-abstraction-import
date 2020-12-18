28acebddf29e6ee2c2c0ee4e97c1f235cc3d4e9e
Tim <tim@tamimi.dev>
2020-12-18 12:58:47 +0000

    Update licence.js

--- src/modules/licence-import/transform/mappers/licence.js
+++ src/modules/licence-import/transform/mappers/licence.js
@@ -27,7 +27,9 @@ const getRegionData = licenceData => {
   const historicalAreaCode = licenceData.AREP_AREA_CODE;
   const regionPrefix = licenceData.AREP_EIUC_CODE.substr(0, 2);
   const regionalChargeArea = regions[regionPrefix];
-  return { historicalAreaCode, regionalChargeArea };
+  const SUC = licenceData.AREP_SUC_CODE;
+  const LEAP = licenceData.AREP_LEAP_CODE;
+  return { historicalAreaCode, regionalChargeArea, SUC, LEAP };
 };
 
 const isNotDraftLicenceVersion = licenceVersion => licenceVersion.STATUS !== 'DRAFT';
