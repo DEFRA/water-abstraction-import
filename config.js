624457bf4c30c7c30d845458b7b58798a0831e4b
James Carmichael <james@siteclick.co.uk>
2021-03-23 12:55:03 +0000

    Disable import of document billing roles from NALD

--- config.js
+++ config.js
@@ -80,7 +80,12 @@ module.exports = {
       schedule: isProduction ? '0 4 * * 1,2,3,4,5' : '0 16 * * 1,2,3,4,5',
       isInvoiceAccountImportEnabled: true,
       isLicenceAgreementImportEnabled: true,
-      isBillingDocumentRoleImportEnabled: true
+      // Note: we think a solution is needed where a list of billing contacts
+      // for a given licence is calculated from the charge version history
+      // in the water service, and synced to CRM v2.
+      // This will supersede the implementation here were the billing contact history
+      // was calculated from NALD data
+      isBillingDocumentRoleImportEnabled: false
     },
     charging: {
       schedule: isProduction ? '0 2 * * 1,2,3,4,5' : '0 14 * * 1,2,3,4,5'
