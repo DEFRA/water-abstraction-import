726cce46a0f710c06398b89b86a5c027b242e7de
Tim <tim@tamimi.dev>
2021-05-05 12:59:24 +0100

    Removing unnecessary conditional

--- src/modules/licence-import/transform/company.js
+++ src/modules/licence-import/transform/company.js
@@ -12,18 +12,16 @@ const mapContactData = data => ({
 });
 
 const transformCompany = companyData => {
-  if (companyData) {
-    const contact = mappers.contact.mapContact(companyData.party);
-    const company = mappers.company.mapCompany(companyData.party);
+  const contact = mappers.contact.mapContact(companyData.party);
+  const company = mappers.company.mapCompany(companyData.party);
 
-    const context = mapContactData(companyData);
+  const context = mapContactData(companyData);
 
-    company.invoiceAccounts = mappers.invoiceAccount.mapInvoiceAccounts(companyData.invoiceAccounts, context);
-    company.addresses = mappers.companyAddress.mapCompanyAddresses(companyData.licenceVersions, companyData.invoiceAccounts, companyData.licenceRoles, context);
-    company.contacts = mappers.companyContact.mapCompanyContacts(contact, companyData.licenceVersions, companyData.invoiceAccounts);
+  company.invoiceAccounts = mappers.invoiceAccount.mapInvoiceAccounts(companyData.invoiceAccounts, context);
+  company.addresses = mappers.companyAddress.mapCompanyAddresses(companyData.licenceVersions, companyData.invoiceAccounts, companyData.licenceRoles, context);
+  company.contacts = mappers.companyContact.mapCompanyContacts(contact, companyData.licenceVersions, companyData.invoiceAccounts);
 
-    return mappers.licence.omitNaldData(company);
-  }
+  return mappers.licence.omitNaldData(company);
 };
 
 exports.transformCompany = transformCompany;
