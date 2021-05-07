571b4e2469d8be56d25eec70a53e1d7def2ec3c3
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-05-07 13:16:01 +0100

    bugfix/water-3041 -- new notify connector to send via water service

---
+++ src/lib/connectors/water/notify.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { serviceRequest } = require('@envage/water-abstraction-helpers');
+const urlJoin = require('url-join');
+
+const config = require('../../../../config');
+
+const getUrl = key => {
+  return urlJoin(config.services.water, 'notify', key);
+};
+
+const postSendNotify = (key, data) => {
+  const url = getUrl(key);
+  return serviceRequest.post(url, {
+    body: data
+  });
+};
+
+exports.postSendNotify = postSendNotify;
