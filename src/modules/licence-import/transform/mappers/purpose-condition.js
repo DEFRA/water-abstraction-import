46388cdbf00350d97d9d159246fb3ca1d6651845
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-05-28 09:50:01 +0100

    bugfix/importing-licence-purpose-conditions -- nald NULL text mapped to propper null

--- src/modules/licence-import/transform/mappers/purpose-condition.js
+++ src/modules/licence-import/transform/mappers/purpose-condition.js
@@ -1,12 +1,19 @@
 'use strict';
+const nald = require('@envage/water-abstraction-helpers').nald;
 
 const mapPurposeConditionFromNALD = data => {
   return {
     code: data.ACIN_CODE,
     subcode: data.ACIN_SUBCODE,
-    param1: data.PARAM1,
-    param2: data.PARAM2,
-    notes: data.TEXT,
+    param1: nald.stringNullToNull(data.PARAM1) === null
+      ? null
+      : data.PARAM1,
+    param2: nald.stringNullToNull(data.PARAM2) === null
+      ? null
+      : data.PARAM2,
+    notes: nald.stringNullToNull(data.TEXT) === null
+      ? null
+      : data.TEXT,
     purposeExternalId: `${data.FGAC_REGION_CODE}:${data.AABP_ID}`,
     externalId: `${data.ID}:${data.FGAC_REGION_CODE}:${data.AABP_ID}`
   };
