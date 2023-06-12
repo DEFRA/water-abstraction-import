759cc9983c4ac7eb9b800f51cdb8a08bac083bca
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-06-12 13:12:21 +0100

    Tidy up of the returns controller
    Before we strip out the legacy logger for our own sanity we just wanted to tidy things up which mainly involved making it clear what was a private method and what was an actual controller.

--- src/modules/returns/controller.js
+++ src/modules/returns/controller.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Boom = require('@hapi/boom')
+
 const { lines, returns, versions } = require('../../lib/connectors/returns')
 const { events } = require('../../lib/connectors/water/events')
 const {
@@ -10,56 +11,9 @@ const {
   filterLines
 } = require('./lib/transformers')
 const { generateNilLines } = require('./lib/generate-nil-lines')
-const { logger } = require('../../logger')
-
 const { getVersionFilter, getEventFilter, getPagination } = require('./lib/api-helpers')
 
-/**
- * Gets all the current versions that have a created_date
- * after the 'start' query params.
- *
- * Can optionally supply an 'end' query param to cap the range.
- */
-const getVersions = async (request, h) => {
-  try {
-    const filter = getVersionFilter(request)
-    const pagination = getPagination(request)
-    return versions.findMany(filter, {}, pagination, ['version_id', 'return_id', 'nil_return'])
-  } catch (err) {
-    logger.error('getVersions error', err.stack)
-    throw err
-  }
-}
-
-const firstItemOrNotFound = (id, { data }) => {
-  if (data.length === 0) {
-    throw Boom.notFound(`Data not found for ${id}`)
-  }
-  return data[0]
-}
-
-const getVersion = async versionId => {
-  const response = await versions.findMany({ version_id: versionId })
-  return firstItemOrNotFound(versionId, response)
-}
-
-const getReturn = async returnId => {
-  const response = await returns.findMany({ return_id: returnId })
-  return firstItemOrNotFound(returnId, response)
-}
-
-const getLines = async versionId => {
-  const filter = { version_id: versionId }
-  const pagination = { perPage: 2000 }
-  const { data } = await lines.findMany(filter, {}, pagination)
-  return data
-}
-
-const getLinesTransformer = returnData => {
-  return returnData.returns_frequency === 'week'
-    ? transformWeeklyLine
-    : transformLine
-}
+const { logger } = require('../../logger')
 
 /**
  * Gets an object containing all line information for a given
@@ -74,12 +28,12 @@ const getLinesForVersion = async (request, h) => {
 
   try {
     const [version, linesResponse] = await Promise.all([
-      getVersion(versionID),
-      getLines(versionID)
+      _version(versionID),
+      _lines(versionID)
     ])
 
-    const returnData = await getReturn(version.return_id)
-    const linesTransformer = getLinesTransformer(returnData)
+    const returnData = await _return(version.return_id)
+    const linesTransformer = _linesTransformer(returnData)
     const linesData = version.nil_return ? generateNilLines(returnData, version) : linesResponse
     const lines = linesData.flatMap(linesTransformer)
 
@@ -99,10 +53,21 @@ const getLinesForVersion = async (request, h) => {
   }
 }
 
-const fetchReturn = async (row) => {
-  const returnId = row['?column?']
-  const data = await getReturn(returnId)
-  return transformReturn(data, ['return_id'])
+/**
+ * Gets all the current versions that have a created_date
+ * after the 'start' query params.
+ *
+ * Can optionally supply an 'end' query param to cap the range.
+ */
+const getVersions = async (request, h) => {
+  try {
+    const filter = getVersionFilter(request)
+    const pagination = getPagination(request)
+    return versions.findMany(filter, {}, pagination, ['version_id', 'return_id', 'nil_return'])
+  } catch (err) {
+    logger.error('getVersions error', err.stack)
+    throw err
+  }
 }
 
 /**
@@ -116,7 +81,7 @@ const getReturns = async (request, h) => {
 
     const response = await events.findMany(filter, {}, pagination, ['metadata->>returnId'])
 
-    const tasks = response.data.map(row => fetchReturn(row))
+    const tasks = response.data.map(row => _fetchReturn(row))
 
     response.data = await Promise.all(tasks)
     return response
@@ -126,8 +91,44 @@ const getReturns = async (request, h) => {
   }
 }
 
+const _fetchReturn = async (row) => {
+  const returnId = row['?column?']
+  const data = await _return(returnId)
+  return transformReturn(data, ['return_id'])
+}
+
+const _firstItemOrNotFound = (id, { data }) => {
+  if (data.length === 0) {
+    throw Boom.notFound(`Data not found for ${id}`)
+  }
+  return data[0]
+}
+
+const _lines = async versionId => {
+  const filter = { version_id: versionId }
+  const pagination = { perPage: 2000 }
+  const { data } = await lines.findMany(filter, {}, pagination)
+  return data
+}
+
+const _linesTransformer = returnData => {
+  return returnData.returns_frequency === 'week'
+    ? transformWeeklyLine
+    : transformLine
+}
+
+const _return = async returnId => {
+  const response = await returns.findMany({ return_id: returnId })
+  return _firstItemOrNotFound(returnId, response)
+}
+
+const _version = async versionId => {
+  const response = await versions.findMany({ version_id: versionId })
+  return _firstItemOrNotFound(versionId, response)
+}
+
 module.exports = {
-  getVersions,
   getLinesForVersion,
+  getVersions,
   getReturns
 }
