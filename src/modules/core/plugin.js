575e25598328400927e7f147a4d02f54251cf707
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-06-13 22:44:24 +0100

    Make import tracker configurable

--- src/modules/core/plugin.js
+++ src/modules/core/plugin.js
@@ -5,14 +5,12 @@ const importTrackerJob = require('./jobs/import-tracker')
 
 const config = require('../../../config')
 
-const getSchedule = () => config.isProduction ? '0 10 * * 1,2,3,4,5' : '0 15 * * 1,2,3,4,5'
-
 const registerSubscribers = async server => {
   await server.messageQueue.subscribe(importTrackerJob.jobName, importTrackerJob.handler)
 
   // If we're not running the unit tests, schedule the import tracker job
   if (process.env.NODE_ENV !== 'test') {
-    cron.schedule(getSchedule(), async () => {
+    cron.schedule(config.import.tracker.schedule, async () => {
       await server.messageQueue.publish(importTrackerJob.createMessage())
     })
   }
