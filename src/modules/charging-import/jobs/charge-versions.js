112f183f0da19f725dcef2e10f8c6a5b208efe98
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-06-16 10:48:53 +0100

    Update getNonDraftChargeVersionsForLicence query
    We learnt in the previous change that this query is used to populate the `water_import.charge_version_metadata` table and it's the content of that table which drives what charge versions get imported.
    
    So, we revert our logic in how to set the scheme and add a new where clauses to only return NALD charge versions with a start date in the PRESROC period. This means the source data for the charge versions import will only be PRESROC based.

--- src/modules/charging-import/jobs/charge-versions.js
+++ src/modules/charging-import/jobs/charge-versions.js
@@ -1,7 +1,20 @@
 'use strict'
 
 /**
- * @note: this can be removed following go live and migration to service
+ * Creates new PRESROC `water.charge_versions` records or updates existing ones based on `import.NALD_CHG_VERSIONS`
+ * @module ChargeVersionsJob
+ *
+ * The job was written as a one-off to create the charge version records WRLS billing needed when it first went live.
+ * It was written at a time there was only the PRESROC scheme.
+ *
+ * Now we have SROC and this process (creating the `water_import.charge_versions_metadata` source data and then
+ * updating `water.charge_versions` using it) has no knowledge of it. Nor does NALD for that matter. A completely
+ * different one-off mechanism (the charge version upload in water-abstraction-service) was built to handle creating
+ * the SROC charge versions needed to support SROC billing.
+ *
+ * When devs and testers build or rebuild a local environment you still need to manually trigger this redundant import
+ * job to create your base PRESROC charge versions. But it only deals with PRESROC. Any licences with a start date
+ * after 2022-04-01 will need you to manually create their charge versions.
  */
 
 const job = require('../lib/job')
