46b5488f10552420cc8d7e098bedab545ff05bd4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-07-04 12:35:37 +0100

    Add logging to NALD import licence jobs
    https://eaflood.atlassian.net/browse/WATER-4024
    https://eaflood.atlassian.net/browse/WATER-4039
    
    As part of our efforts to [clean up the logs](https://github.com/DEFRA/water-abstraction-import/pull/654) for the import service we removed the logging that was happening in `src/modules/nald-import/jobs/import-licence.js`.
    
    One job gets added for every licence to import which is normally 71K! Since then we've been looking at the output to confirm we can see what is going on.
    
    We found there was a big unknown when it came to the NALD import licence job. With the logging removed there was no way to confirm the jobs were running. We didn't want to put the logging back in. But also, we were limited with what we could do. Later versions of PGBoss allow you to query the queue and jobs remaining but our version is too old and out of scope for being updated.
    
    So, the solution we have come up with is to added data on how many invoices there are to import, along with the index position of each one to the job's data. We can then use this information to decide whether to output a start and finish log message that confirms to us this process is happening.
    
    For reference, the import can take _up to 3 hours!_ So, when you are checking the logs be patient waiting for the 'finished' entry 😬

--- src/modules/nald-import/jobs/populate-pending-import-complete.js
+++ src/modules/nald-import/jobs/populate-pending-import-complete.js
@@ -5,9 +5,17 @@ const importLicenceJob = require('./import-licence')
 async function handler (messageQueue, job) {
   if (!job.failed) {
     const { licenceNumbers } = job.data.response
+    const numberOfLicences = licenceNumbers.length
 
-    for (const licenceNumber of licenceNumbers) {
-      await messageQueue.publish(importLicenceJob.createMessage(licenceNumber))
+    for (const [index, licenceNumber] of licenceNumbers.entries()) {
+      // This information is to help us log when the import licence jobs start and finish. See
+      // src/modules/nald-import/jobs/import-licence.js for more details
+      const data = {
+        licenceNumber,
+        jobNumber: index + 1,
+        numberOfLicences
+      }
+      await messageQueue.publish(importLicenceJob.createMessage(data))
     }
   }
 
