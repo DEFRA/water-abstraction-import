830b9c91f12d9e92913d5ea0ecf915e394ac0688
Roble <93697715+rvsiyad@users.noreply.github.com>
2024-01-15 10:26:45 +0000

    Removing console.log() from legacy migrations (#825)
    * Removing console.log() from legacy migrations
    
    https://github.com/DEFRA/water-abstraction-team/issues/91
    
    This PR is focusing on removing console.log() from legacy migrations as per Teams issue. When working on DEFRA/water-abstraction-service#2227 we again broke GitHub CI because of the volume of output from our migrations.
    
    In DEFRA/water-abstraction-team#65 we thought we'd dealt with the issue believing that db-mgrate's verbose() argument was to blame.
    
    Looking into the issue in the stuck bill run we came across a db-migrate/node-db-migrate#453 (comment) that gave us a 🤦 !
    
    The db-migrate template used when generating migrations includes a console.log() that outputs the migration file read in. So, though we might not be outputting all the SQL being fired, we are still outputting the contents of every single migration file!
    
    We definitely don't need to see this and it should remove the chance of us breaking the build in this way once and for all.
    
    So, this issue is about going into the existing migrations and removing the 2 console.log('received data: ' + data) lines in each across all repos.
    
    * Removing console.log() from legacy migrations

--- migrations/20201120135927-create-charge-versions-metadata-table.js
+++ migrations/20201120135927-create-charge-versions-metadata-table.js
@@ -17,7 +17,6 @@ function up (db) {
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
       if (err) return reject(err)
-      console.log('received data: ' + data)
 
       resolve(data)
     })
@@ -32,7 +31,6 @@ function down (db) {
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
       if (err) return reject(err)
-      console.log('received data: ' + data)
 
       resolve(data)
     })
