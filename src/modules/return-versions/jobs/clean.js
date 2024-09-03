9397d218793e700ecd1ea30b033625418ff5d053
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-09-03 12:18:20 +0100

    Add clean step into return versions job
    The step isn't doing anything at the moment. But this allows us to check that clean can be triggered followed by the import (it can!)

---
+++ src/modules/return-versions/jobs/clean.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const { pool } = require('../../../lib/connectors/db.js')
+const Queries = require('../lib/clean-queries.js')
+const ImportJob = require('./import.js')
+
+const JOB_NAME = 'return-versions.clean'
+
+function createMessage () {
+  return {
+    name: JOB_NAME,
+    options: {
+      singletonKey: JOB_NAME
+    }
+  }
+}
+
+async function handler () {
+  try {
+    global.GlobalNotifier.omg(`${JOB_NAME}: started`)
+  } catch (error) {
+    global.GlobalNotifier.omfg(`${JOB_NAME}: errored`, error)
+    throw error
+  }
+}
+
+async function onComplete (messageQueue, job) {
+  if (!job.failed) {
+    await messageQueue.publish(ImportJob.createMessage())
+
+    global.GlobalNotifier.omg(`${JOB_NAME}: finished`)
+  } else {
+    global.GlobalNotifier.omg(`${JOB_NAME}: failed`)
+  }
+}
+
+module.exports = {
+  JOB_NAME,
+  createMessage,
+  handler,
+  onComplete
+}
