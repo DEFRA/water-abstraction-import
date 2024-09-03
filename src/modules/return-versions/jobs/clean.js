72ef88d4eda905a3c8219b4860315b2e52ce4f14
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-09-03 15:43:09 +0100

    Add the clean up queries

--- src/modules/return-versions/jobs/clean.js
+++ src/modules/return-versions/jobs/clean.js
@@ -18,6 +18,21 @@ function createMessage () {
 async function handler () {
   try {
     global.GlobalNotifier.omg(`${JOB_NAME}: started`)
+
+    // Delete any return requirement points linked to deleted NALD return requirements
+    await pool.query(Queries.cleanPoints)
+
+    // Delete any return requirement purposes linked to deleted NALD return requirements
+    await pool.query(Queries.cleanPurposes)
+
+    // Delete any return requirements linked to deleted NALD return requirements
+    await pool.query(Queries.cleanRequirements)
+
+    // Delete any return versions that have no return requirements and that are linked to deleted return versions
+    await pool.query(Queries.cleanVersions)
+
+    // Update the mod logs to remove the return version ID for where the return version has now been deleted
+    await pool.query(Queries.cleanModLogs)
   } catch (error) {
     global.GlobalNotifier.omfg(`${JOB_NAME}: errored`, error)
     throw error
--- src/modules/return-versions/lib/clean-queries.js
+++ src/modules/return-versions/lib/clean-queries.js
@@ -1,3 +1,128 @@
 'use strict'
 
-module.exports = {}
+const cleanPoints = `
+  WITH nald_return_requirements AS (
+    SELECT concat_ws(':', nrf."FGAC_REGION_CODE", nrf."ID") AS nald_id
+    FROM "import"."NALD_RET_FORMATS" nrf
+  )
+  DELETE FROM water.return_requirement_points rrp WHERE rrp.return_requirement_id IN (
+    SELECT
+      rr.return_requirement_id
+    FROM
+      water.return_requirements rr
+    WHERE
+      NOT EXISTS (
+        SELECT 1
+        FROM nald_return_requirements nrr
+        WHERE rr.external_id = nrr.nald_id
+      )
+      AND NOT EXISTS (
+        SELECT 1
+        FROM "returns"."returns" rl
+        WHERE
+          rl.return_requirement = rr.legacy_id::varchar
+        LIMIT 1
+      )
+  );
+`
+
+const cleanPurposes = `
+  WITH nald_return_requirements AS (
+    SELECT concat_ws(':', nrf."FGAC_REGION_CODE", nrf."ID") AS nald_id
+    FROM "import"."NALD_RET_FORMATS" nrf
+  )
+  DELETE FROM water.return_requirement_purposes rrp WHERE rrp.return_requirement_id IN (
+    SELECT
+      rr.return_requirement_id
+    FROM
+      water.return_requirements rr
+    WHERE
+      NOT EXISTS (
+        SELECT 1
+        FROM nald_return_requirements nrr
+        WHERE rr.external_id = nrr.nald_id
+      )
+      AND NOT EXISTS (
+        SELECT 1
+        FROM "returns"."returns" rl
+        WHERE
+          rl.return_requirement = rr.legacy_id::varchar
+        LIMIT 1
+      )
+  );
+`
+
+const cleanRequirements = `
+  WITH nald_return_requirements AS (
+    SELECT concat_ws(':', nrf."FGAC_REGION_CODE", nrf."ID") AS nald_id
+    FROM "import"."NALD_RET_FORMATS" nrf
+  )
+  DELETE FROM water.return_requirements WHERE return_requirement_id IN (
+    SELECT
+      rr.return_requirement_id
+    FROM
+      water.return_requirements rr
+    WHERE
+      NOT EXISTS (
+        SELECT 1
+        FROM nald_return_requirements nrr
+        WHERE rr.external_id = nrr.nald_id
+      )
+      AND NOT EXISTS (
+        SELECT 1
+        FROM "returns"."returns" rl
+        WHERE
+          rl.return_requirement = rr.legacy_id::varchar
+        LIMIT 1
+      )
+  );
+`
+
+const cleanVersions = `
+  WITH nald_return_versions AS (
+    SELECT concat_ws(':', nv."FGAC_REGION_CODE", nv."AABL_ID", nv."VERS_NO") AS nald_id
+    FROM "import"."NALD_RET_VERSIONS" nv
+  )
+  DELETE FROM water.return_versions WHERE return_version_id IN (
+    SELECT
+      rv.return_version_id
+    FROM
+      water.return_versions rv
+    WHERE
+      NOT EXISTS (
+        SELECT 1
+        FROM nald_return_versions nrv
+        WHERE rv.external_id = nrv.nald_id
+      )
+      AND NOT EXISTS (
+        SELECT 1
+        FROM water.return_requirements rr
+        WHERE
+          rr.return_version_id = rv.return_version_id
+        LIMIT 1
+      )
+  );
+`
+
+const cleanModLogs = `
+  UPDATE
+    water.mod_logs ml
+  SET
+    return_version_id = NULL
+  WHERE
+    ml.return_version_id IS NOT NULL
+    AND ml.return_version_id NOT IN (
+      SELECT
+        rv.return_version_id
+      FROM
+        water.return_versions rv
+    );
+`
+
+module.exports = {
+  cleanPoints,
+  cleanPurposes,
+  cleanRequirements,
+  cleanVersions,
+  cleanModLogs
+}
