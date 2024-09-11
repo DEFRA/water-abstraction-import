7f666641c212f9fba9d852ac9aa8a7850f2ca5b2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-09-11 17:42:10 +0100

    Drop dependence on repository
    The Repository class looks to have been intended to have a generic way to generate queries to the DB using config.
    
    Vastly complex though, and completely unnecessary, especially as we know the 2 insert queries needed.
    
    Removed in an effort to simplify the solution and eek out performance improvements.

--- src/lib/general.js
+++ src/lib/general.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { randomUUID } = require('crypto')
+
 /**
  * Calculates and logs the time taken in milliseconds between the provided `startTime` and the current time
  *
@@ -61,7 +63,28 @@ function currentTimeInNanoseconds () {
   return process.hrtime.bigint()
 }
 
+/**
+ * Generate a Universally Unique Identifier (UUID)
+ *
+ * The service uses these as the IDs for most records in the DB. Most tables will automatically generate them when
+ * the record is created but not all do. There are also times when it is either more performant, simpler, or both for
+ * us to generate the ID before inserting a new record. For example, we can pass the generated ID to child records to
+ * set the foreign key relationship.
+ *
+ * NOTE: We set `disableEntropyCache` to `false` as normally, for performance reasons node caches enough random data to
+ * generate up to 128 UUIDs. We disable this as we may need to generate more than this and the performance hit in
+ * disabling this cache is a rounding error in comparison to the rest of the process.
+ *
+ * https://nodejs.org/api/crypto.html#cryptorandomuuidoptions
+ *
+ * @returns {string} a randomly generated UUID
+ */
+function generateUUID () {
+  return randomUUID({ disableEntropyCache: true })
+}
+
 module.exports = {
   calculateAndLogTimeTaken,
-  currentTimeInNanoseconds
+  currentTimeInNanoseconds,
+  generateUUID
 }
--- src/modules/permit-import/lib/loader.js
+++ src/modules/permit-import/lib/loader.js
@@ -1,49 +1,9 @@
 'use strict'
 
-const { v4: uuidV4 } = require('uuid')
-
-const { pool } = require('../../../lib/connectors/db.js')
-const Repository = require('../../../lib/repository.js')
+const db = require('../../../lib/connectors/db.js')
+const { generateUUID } = require('../../../lib/general.js')
 const { buildCRMPacket } = require('./transform-crm')
 const { getLicenceJson, buildPermitRepoPacket } = require('./transform-permit')
-
-async function _loadDocumentHeader (licenceNumber, licenceData, permitRepoId) {
-  const crmData = buildCRMPacket(licenceData, licenceNumber, permitRepoId)
-
-  // Legacy madness! please don't ask us why someone built this and only used it for importing a few tables
-  const repository = new Repository(
-    pool,
-    {
-      table: 'crm.document_header',
-      upsert: {
-        fields: ['system_id', 'system_internal_id', 'regime_entity_id'],
-        set: ['system_external_id', 'metadata']
-      }
-    }
-  )
-
-  return repository.persist({ document_id: uuidV4(), ...crmData })
-}
-
-async function _loadPermit (licenceNumber, licenceData) {
-  const permit = buildPermitRepoPacket(licenceNumber, 1, 8, licenceData)
-
-  // Legacy madness! please don't ask us why someone built this and only used it for importing a few tables
-  const repository = new Repository(
-    pool,
-    {
-      table: 'permit.licence',
-      upsert: {
-        fields: ['licence_regime_id', 'licence_type_id', 'licence_ref'],
-        set: ['licence_status_id', 'licence_search_key', 'is_public_domain', 'licence_start_dt', 'licence_end_dt', 'licence_data_value']
-      }
-    }
-  )
-
-  const { rows: [{ licence_id: permitRepoId }] } = await repository.persist(permit, ['licence_id'])
-
-  return permitRepoId
-}
 
 async function load (licenceNumber) {
   const licenceData = await getLicenceJson(licenceNumber)
@@ -55,6 +15,92 @@ async function load (licenceNumber) {
   const permitRepoId = await _loadPermit(licenceNumber, licenceData)
 
   return _loadDocumentHeader(licenceNumber, licenceData, permitRepoId)
+}
+
+async function _loadDocumentHeader (licenceNumber, licenceData, permitRepoId) {
+  const crmData = buildCRMPacket(licenceData, licenceNumber, permitRepoId)
+
+  return _persistDocumentHeader(crmData)
+}
+
+async function _loadPermit (licenceNumber, licenceData) {
+  const permit = buildPermitRepoPacket(licenceNumber, 1, 8, licenceData)
+
+  const results = await _persistPermit(permit)
+
+  return results[0].licence_id
+}
+
+async function _persistDocumentHeader (documentHeader) {
+  const params = [
+    generateUUID(),
+    documentHeader.regime_entity_id,
+    documentHeader.system_id,
+    documentHeader.system_internal_id,
+    documentHeader.system_external_id,
+    documentHeader.metadata
+  ]
+
+  const query = `
+    INSERT INTO crm.document_header (
+      document_id,
+      regime_entity_id,
+      system_id,
+      system_internal_id,
+      system_external_id,
+      metadata
+    )
+    VALUES ($1, $2, $3, $4, $5, $6)
+    ON CONFLICT (
+      system_id,
+      system_internal_id,
+      regime_entity_id
+    )
+    DO UPDATE SET
+      system_external_id = EXCLUDED.system_external_id,
+      metadata = EXCLUDED.metadata;
+  `
+
+  return db.query(query, params)
+}
+
+async function _persistPermit (permit) {
+  const params = [
+    permit.licence_ref,
+    permit.licence_start_dt,
+    permit.licence_end_dt,
+    permit.licence_status_id,
+    permit.licence_type_id,
+    permit.licence_regime_id,
+    permit.licence_data_value
+  ]
+
+  const query = `
+    INSERT INTO permit.licence (
+      licence_ref,
+      licence_start_dt,
+      licence_end_dt,
+      licence_status_id,
+      licence_type_id,
+      licence_regime_id,
+      licence_data_value
+    )
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    ON CONFLICT (
+      licence_regime_id,
+      licence_type_id,
+      licence_ref
+    ) DO UPDATE SET
+      licence_status_id = EXCLUDED.licence_status_id,
+      licence_search_key = EXCLUDED.licence_search_key,
+      is_public_domain = EXCLUDED.is_public_domain,
+      licence_start_dt = EXCLUDED.licence_start_dt,
+      licence_end_dt = EXCLUDED.licence_end_dt,
+      licence_data_value = EXCLUDED.licence_data_value
+    RETURNING licence_id;
+  `
+
+  return db.query(query, params)
 }
 
 module.exports = {
