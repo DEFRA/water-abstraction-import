983719316de7e7c8e9fb63947f427e7f4b9f2abc
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-09-13 18:35:27 +0100

    Remove the queue and import system job
    We'll need to think of an alternate approach. But till then we can simply remove them.

--- src/modules/licence-import/plugin.js
+++ src/modules/licence-import/plugin.js
@@ -1,22 +1,10 @@
 'use strict'
 
 const ImportLicenceJob = require('./jobs/import-licence.js')
-const ImportLicenceSystemJob = require('./jobs/import-licence-system.js')
 const ImportPointsJob = require('./jobs/import-points.js')
 const QueueLicencesJob = require('./jobs/queue-licences.js')
-const QueueLicencesSystemJob = require('./jobs/queue-licences-system.js')
 
 async function register (server, _options) {
-  await server.messageQueue.subscribe(QueueLicencesSystemJob.name, QueueLicencesSystemJob.handler)
-  await server.messageQueue.onComplete(QueueLicencesSystemJob.name, (executedJob) => {
-    return QueueLicencesSystemJob.onComplete(server.messageQueue, executedJob)
-  })
-
-  await server.messageQueue.subscribe(ImportLicenceSystemJob.name, ImportLicenceSystemJob.options, ImportLicenceSystemJob.handler)
-  await server.messageQueue.onComplete(ImportLicenceSystemJob.name, (executedJob) => {
-    return ImportLicenceSystemJob.onComplete(server.messageQueue, executedJob)
-  })
-
   await server.messageQueue.subscribe(QueueLicencesJob.name, QueueLicencesJob.handler)
   await server.messageQueue.onComplete(QueueLicencesJob.name, (executedJob) => {
     return QueueLicencesJob.onComplete(server.messageQueue, executedJob)
