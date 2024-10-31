ce98f8267a3675e188612cb903af6bb580e3f7dd
Jason Claxton <jason.claxton@defra.gov.uk>
2024-10-31 13:40:46 +0000

    Put new cleaning functionality behind a feature flag

--- src/modules/licence-import/jobs/clean.js
+++ src/modules/licence-import/jobs/clean.js
@@ -23,11 +23,13 @@ async function handler () {
     // Mark records in crm_v2.documents as deleted if the licence numbers no longer exist in import.NALD_ABS_LICENCES
     await pool.query(Queries.cleanCrmV2Documents)
 
-    // Delete any licence monitoring stations linked to deleted NALD licence version purpose conditions
-    await pool.query(Queries.cleanLicenceMonitoringStations)
+    if (process.env.CLEAN_LICENCE_IMPORTS === 'true') {
+      // Delete any licence monitoring stations linked to deleted NALD licence version purpose conditions
+      await pool.query(Queries.cleanLicenceMonitoringStations)
 
-    // Delete any licence version purpose conditions linked to deleted NALD licence version purpose conditions
-    await pool.query(Queries.cleanLicenceVersionPurposeConditions)
+      // Delete any licence version purpose conditions linked to deleted NALD licence version purpose conditions
+      await pool.query(Queries.cleanLicenceVersionPurposeConditions)
+    }
   } catch (error) {
     global.GlobalNotifier.omfg(`${JOB_NAME}: errored`, error)
     throw error
