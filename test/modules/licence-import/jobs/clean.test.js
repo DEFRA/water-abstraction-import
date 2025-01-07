e43a3f636c0a6f81d2b6001207d1e805e1d023c0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-01-07 13:14:31 +0000

    Update the Clean job
    Have it queue up the trigger end date process rather than import-purpose-condition-types.

--- test/modules/licence-import/jobs/clean.test.js
+++ test/modules/licence-import/jobs/clean.test.js
@@ -109,12 +109,12 @@ experiment('Licence Import: Clean', () => {
         expect(message).to.equal('licence-import.clean: finished')
       })
 
-      test('the import purpose condition types job is published to the queue', async () => {
+      test('the trigger end date process job is published to the queue', async () => {
         await CleanJob.onComplete(messageQueue, job)
 
         const jobMessage = messageQueue.publish.lastCall.args[0]
 
-        expect(jobMessage.name).to.equal('licence-import.import-purpose-condition-types')
+        expect(jobMessage.name).to.equal('licence-import.trigger-end-date-process')
       })
 
       experiment('but an error is thrown', () => {
