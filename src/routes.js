185af44b3364ce4923bfd7dcc348a71b63c79b5f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-01-31 15:30:07 +0000

    Register our manual route with the server

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const jobSummaryRoutes = require('./modules/jobs/routes')
 const licenceImportRoutes = require('./modules/licence-import/routes')
 const naldImportRoutes = require('./modules/nald-import/routes')
 const returnsRoutes = require('./modules/returns/routes')
+const returnLogsRoutes = require('./modules/return-logs/routes')
 const returnVersionsRoutes = require('./modules/return-versions/routes.js')
 const modLogsRoutes = require('./modules/mod-logs/routes.js')
 const pointsRoutes = require('./modules/points/routes.js')
@@ -17,6 +18,7 @@ module.exports = [
   ...licenceImportRoutes,
   ...naldImportRoutes,
   ...returnsRoutes,
+  ...returnLogsRoutes,
   ...returnVersionsRoutes,
   ...modLogsRoutes,
   ...pointsRoutes
