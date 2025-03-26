0e7af8f973c0922c92f4f00d2c0761c5484e436f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-03-26 17:02:58 +0000

    Reformat job completion email
    Realised that the current content not only required an overly complex query, it didn't really tell us anything.
    
    With the reformat now we can see each step that was processed, when it was started, and how long it took.
    
    It might also help those who receive the email get a better sense of what the overnight import is actually doing.

--- src/modules/completion-email/process.js
+++ src/modules/completion-email/process.js
@@ -1,10 +1,9 @@
 'use strict'
 
-const moment = require('moment')
 const { NotifyClient } = require('notifications-node-client')
 
 const db = require('../../lib/connectors/db.js')
-const { currentTimeInNanoseconds, calculateAndLogTimeTaken } = require('../../lib/general.js')
+const { currentTimeInNanoseconds, calculateAndLogTimeTaken, formatLongDateTime } = require('../../lib/general.js')
 
 const config = require('../../../config.js')
 
@@ -12,9 +11,9 @@ async function go (log = false) {
   try {
     const startTime = currentTimeInNanoseconds()
 
-    const failedJobs = await _failedJobsSummary()
+    const jobs = await _jobs()
 
-    const emailOptions = _emailOptions(failedJobs)
+    const emailOptions = _emailOptions(jobs)
 
     await _sendEmail(emailOptions)
 
@@ -26,83 +25,86 @@ async function go (log = false) {
   }
 }
 
-function _emailOptions (failedJobs) {
-  const options = {
-    personalisation: {
-      content: `The ${config.environment} environment reported no failures with the overnight import job.`
-    }
-  }
+function _detail (jobs) {
+  const details = jobs.map((job) => {
+    const { createdOn, name, state, timeTaken } = job
 
-  if (failedJobs.length === 0) {
-    return options
-  }
+    const startTime = createdOn.toLocaleTimeString('en-GB')
+    const stepName = name.replace('import-job.', '')
+    const stepDuration = timeTaken ? ` (${timeTaken.toPostgres()})` : null
 
-  const reportRows = []
-  for (const failedJob of failedJobs) {
-    const { dateCompleted, dateCreated, jobName, total } = failedJob
-    const reportRow = `Job Name: ${jobName} \nTotal Errors: ${total} \nDate created: ${dateCreated} \nDate completed: ${dateCompleted}`
+    // NOTE: timeTaken is returned as a PostgresInterval custom type thanks to the pg package and as such has a method
+    // we can call on to nicely format the interval value into something we can read
+    return `* ${startTime} - ${stepName} - ${state}${stepDuration}`
+  })
 
-    reportRows.push(reportRow)
-  }
-
-  const subTitle = `The ${config.environment} environment reported ${failedJobs.length} failures with the overnight import job.`
-
-  options.personalisation.content = `${subTitle}\n\n${reportRows.join('\n\n')}`
-
-  return options
+  return details.join('\n')
 }
 
-async function _failedJobsSummary () {
+function _emailOptions (jobs) {
+  const jobSummary = _jobSummary(jobs)
+  const detail = _detail(jobs)
+  const timeSummary = _timeSummary(jobs)
+
+  return {
+    personalisation: {
+      detail,
+      jobSummary,
+      timeSummary
+    }
+  }
+}
+
+async function _jobs () {
   const results = await db.query(`
     SELECT
-      name,
-      sum(count) AS count,
-      max(max_completed_date) AS max_completed_date,
-      max(max_created_date) AS max_created_date
+      j."name" AS job_name,
+      j.state,
+      j.createdon,
+      j.completedon,
+      age(j.completedon, j.createdon) AS time_taken
     FROM
-      (
-        SELECT
-            name,
-            COUNT(*),
-            max(completedon) AS max_completed_date,
-            max(createdon) AS max_created_date
-        FROM
-            water_import.job j
-        WHERE
-            j.state = 'failed'
-        GROUP BY
-          j.name
-      UNION ALL
-        SELECT
-            name,
-            COUNT(*),
-            max(completedon) AS max_completed_date,
-            max(createdon) AS max_created_date
-        FROM
-            water_import.archive a
-        WHERE
-            a.state = 'failed'
-        GROUP BY
-          a.name
-      ) cte
-    GROUP BY
-      name;
+      water_import.job j
+    WHERE
+      j."name" LIKE 'import-job.%'
+    ORDER BY
+      j.createdon ASC;
   `)
 
   return results.map((row) => {
     return {
-      jobName: row.name,
-      total: row.count,
-      dateCreated: row.max_created_date ? moment(row.max_created_date).format('DD MMM YYYY HH:mm:ss') : '',
-      dateCompleted: row.max_completed_date ? moment(row.max_completed_date).format('DD MMM YYYY HH:mm:ss') : ''
+      completedOn: row.completedon,
+      createdOn: row.createdon,
+      name: row.job_name,
+      state: row.state,
+      timeTaken: row.time_taken
     }
   })
+}
+
+function _jobSummary (jobs) {
+  const failedJobs = jobs.filter((job) => {
+    return job.state === 'failed'
+  })
+
+  if (failedJobs.length === 0) {
+    return `The ${config.environment} environment had no steps fail during the overnight import job.`
+  }
+
+  return `The ${config.environment} environment had ${failedJobs.length} step(s) fail during the overnight import job.`
 }
 
 async function _sendEmail (options) {
   const client = new NotifyClient(config.notify.apiKey)
 
   await client.sendEmail(config.notify.templateId, config.notify.mailbox, options)
+}
+
+function _timeSummary (jobs) {
+  const startTime = formatLongDateTime(jobs[0].createdOn)
+  const endTime = formatLongDateTime(jobs[jobs.length - 1].createdOn)
+
+  return `Started ${startTime} - finished ${endTime}`
 }
 
 module.exports = {
