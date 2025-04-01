bfe602f41f9c257574f235a378a9e99d3bdebe6b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-04-01 16:09:02 +0100

    Upped pmap concurrency to 50

--- src/modules/import-job/lib/licence-data-import.js
+++ src/modules/import-job/lib/licence-data-import.js
@@ -87,7 +87,7 @@ async function _pmapProcess () {
 
   const licences = await _licences()
 
-  const allMessages = await pMap(licences, _processLicence, { concurrency: 20 })
+  const allMessages = await pMap(licences, _processLicence, { concurrency: 50 })
 
   if (config.featureFlags.disableReturnsImports) {
     allMessages.push('Skipped licence-returns-import because importing returns is disabled')
