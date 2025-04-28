9dd484d9a58eb7b81f31bd5d39fccdc6edc06d15
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-04-28 14:32:14 +0100

    Update persist-returns to use the new data source
    When called from the over night job, it will have fetched the return cycles and have passed them through via `process.js`.
    
    This is because the licence-returns-import process is called per licence, and we want to fetch the return cycles only once. But we've added a bit of logic to handle it being empty, so we can still support the process being called by the testing endpoint.

--- test/modules/licence-returns-import/lib/persist-returns.test.js
+++ test/modules/licence-returns-import/lib/persist-returns.test.js
@@ -15,12 +15,22 @@ const db = require('../../../../src/lib/connectors/db.js')
 const PersistReturns = require('../../../../src/modules/licence-returns-import/lib/persist-returns.js')
 
 experiment('modules/licence-returns-import/lib/persist-returns', () => {
+  const returnCycles = {
+    summer: [{
+      end_date: '2017-10-31',
+      is_summer: true,
+      return_cycle_id: '2fc23cfc-6287-49ab-af8c-6c8215504722',
+      start_date: '2016-11-01'
+    }],
+    winter: []
+  }
+
   const naldReturn = {
     due_date: '2017-11-28',
     end_date: '2017-10-31',
     licence_ref: '01/234/567',
     licence_type: 'abstraction',
-    metadata: JSON.stringify({ param: 'value', version: '1' }),
+    metadata: JSON.stringify({ isSummer: true, param: 'value', version: '1' }),
     received_date: '2017-11-24',
     regime: 'water',
     return_id: 'v1:123:456',
@@ -40,14 +50,13 @@ experiment('modules/licence-returns-import/lib/persist-returns', () => {
     beforeEach(() => {
       Sinon.stub(db, 'query')
         .onFirstCall().resolves([{ return_log_exists: false, return_submission_exists: true }])
-        .onSecondCall().resolves([{ return_cycle_id: '40eb9d9e-0cad-4794-b7eb-dfc7ccaf8b26' }])
-        .onThirdCall().resolves()
+        .onSecondCall().resolves()
     })
 
     test('creates the return log', async () => {
-      await PersistReturns.go([naldReturn], false)
+      await PersistReturns.go([naldReturn], false, returnCycles)
 
-      const params = db.query.thirdCall.args[1]
+      const params = db.query.secondCall.args[1]
 
       // Confirm all the params required were passed to the query
       expect(params).to.equal([
@@ -55,17 +64,17 @@ experiment('modules/licence-returns-import/lib/persist-returns', () => {
         '2017-10-31',
         '01/234/567',
         'abstraction',
-        JSON.stringify({ param: 'value', version: '1' }),
+        JSON.stringify({ isSummer: true, param: 'value', version: '1' }),
         '2017-11-24',
         'water',
+        '2fc23cfc-6287-49ab-af8c-6c8215504722',
         'v1:123:456',
         '012345',
         'month',
         '2017-11-01',
         'NALD',
         '2016-11-01',
-        'completed',
-        '40eb9d9e-0cad-4794-b7eb-dfc7ccaf8b26'
+        'completed'
       ])
     })
   })
@@ -77,16 +86,17 @@ experiment('modules/licence-returns-import/lib/persist-returns', () => {
         .onSecondCall().resolves()
     })
 
-    test("updates the return log's 'due_date', 'metadata', 'received_date', 'returns_frequency', 'sent_date', and 'status'", async () => {
-      await PersistReturns.go([naldReturn], false)
+    test("updates the return log's 'due_date', 'metadata', 'received_date', 'return_cycle_id', 'returns_frequency', 'sent_date', and 'status'", async () => {
+      await PersistReturns.go([naldReturn], false, returnCycles)
 
       const params = db.query.secondCall.args[1]
 
       // Confirm all the params required were passed to the query
       expect(params).to.equal([
         '2017-11-28',
-        '{"param":"value","version":"1"}',
+        '{"isSummer":true,"param":"value","version":"1"}',
         '2017-11-24',
+        '2fc23cfc-6287-49ab-af8c-6c8215504722',
         'month',
         '2017-11-01',
         'completed',
