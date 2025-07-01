e40c1eeee50b57fd29ec5dbc0a01af47944ca9bc
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-07-01 22:43:57 +0100

    Change create to drop schema and stop migrations
    Now we use the script that was used to also create the schema first to drop it instead. Being outside of the db-migrate it doesn't try to record the action.
    
    We update the migration command (and drop the others whilst at it) to now only run our `drop-schema.js` script.

--- scripts/drop-schema.js
+++ scripts/drop-schema.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { pool } = require('../src/lib/connectors/db')
 
 async function run () {
-  const { error } = await pool.query('CREATE SCHEMA IF NOT EXISTS water_import;')
+  const { error } = await pool.query('DROP SCHEMA IF EXISTS "water_import" CASCADE;')
   console.log(error || 'OK')
   process.exit(error ? 1 : 0)
 }
