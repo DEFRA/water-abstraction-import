f3c2ca42c5935dde2da0211e7f9c19be118a9b2b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-07-01 23:04:20 +0100

    Drop the table
    We take advantage of the `drop-schema.js` script to perform this task. As soon as we know this has been run in all environments, including production, we'll be deleting `drop-schema.js` anyway.

--- scripts/drop-schema.js
+++ scripts/drop-schema.js
@@ -2,9 +2,17 @@ require('dotenv').config()
 const { pool } = require('../src/lib/connectors/db')
 
 async function run () {
-  const { error } = await pool.query('DROP SCHEMA IF EXISTS "water_import" CASCADE;')
-  console.log(error || 'OK')
-  process.exit(error ? 1 : 0)
+  let result = await pool.query('DROP SCHEMA IF EXISTS "water_import" CASCADE;')
+
+  if (result.error) {
+    console.error(result.error)
+    process.exit(1)
+  }
+
+  result = await pool.query('DROP TABLE IF EXISTS public."NALD_RET_LINES" CASCADE;')
+
+  console.log(result.error || 'OK')
+  process.exit(result.error ? 1 : 0)
 }
 
 run()
