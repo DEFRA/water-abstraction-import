22dc4e685cbfa618867bc887f26857eec07e9906
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-06-04 12:47:11 +0100

    Update process to match where we are up to - WIP
    Process is now fetching the missing void return lines, collating them, and creating return logs where one does not currently exist in WRLS.
    
    Now all our collated return are at a point we can assign the missing lines to them.

--- src/modules/missing-void-returns/process.js
+++ src/modules/missing-void-returns/process.js
@@ -1,12 +1,21 @@
 'use strict'
 
-const { currentTimeInNanoseconds, calculateAndLogTimeTaken } = require('../../lib/general.js')
+const CollateMissingVoidReturnLines = require('./lib/collate-missing-void-return-lines.js')
+const CreateMissingReturnLog = require('./lib/create-missing-return-log.js')
+const FetchMissingVoidReturnLines = require('./lib/fetch-missing-void-return-lines.js')
+const { currentTimeInNanoseconds, calculateAndLogTimeTaken, timestampForPostgres } = require('../../lib/general.js')
 
 async function go (log = false) {
   const messages = []
 
   try {
     const startTime = currentTimeInNanoseconds()
+
+    const missingVoidReturnLines = await FetchMissingVoidReturnLines.go()
+
+    const collatedReturns = CollateMissingVoidReturnLines.go(missingVoidReturnLines)
+
+    await _processMissingReturnLogs(collatedReturns)
 
     if (log) {
       calculateAndLogTimeTaken(startTime, 'missing-void-returns: complete', { messages })
@@ -18,6 +27,21 @@ async function go (log = false) {
   }
 
   return messages
+}
+
+async function _processMissingReturnLogs (collatedReturns) {
+  const missingReturns = collatedReturns.filter((collatedReturn) => {
+    return !collatedReturn.returnLog.id
+  })
+
+  const timestamp = timestampForPostgres()
+
+  for (const missingReturn of missingReturns) {
+    const { id, returnId } = await CreateMissingReturnLog.go(missingReturn, timestamp)
+
+    missingReturn.returnLog.id = id
+    missingReturn.returnLog.returnId = returnId
+  }
 }
 
 module.exports = {
