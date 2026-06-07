a5edebb9babce3396363e5bcf1910778bea66e3e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-06-07 23:43:28 +0100

    Collate the fetched details
    What we fetch is a combination of return requirement details plus each return cycle found to be missing.
    
    We intend to create a new return log for each missed return cycle, but we don't need to fetch the points and purposes for every row.
    
    So, we start by collating the records returned by return requirements ID, grouping the return cycles together.

---
+++ src/modules/missing-return-logs/lib/collate-missing-return-logs.js
@@ -0,0 +1,63 @@
+'use strict'
+
+function go (missingReturnLogs) {
+  const missingReturns = _processReturnLogs(missingReturnLogs)
+
+  return Object.values(missingReturns)
+}
+
+function _processReturnLogs (missingReturnLogs) {
+  const missingReturns = {}
+
+  for (const missingReturnLog of missingReturnLogs) {
+    const { return_cycle_id: returnCycleId, return_requirement_id: returnRequirementId } = missingReturnLog
+
+    const returnCycle = {
+      dueDate: new Date(missingReturnLog.return_cycle_due_date),
+      endDate: new Date(missingReturnLog.return_cycle_end_date),
+      id: returnCycleId,
+      startDate: new Date(missingReturnLog.return_cycle_start_date)
+    }
+
+    if (missingReturns[returnRequirementId]) {
+      missingReturns[returnRequirementId].returnCycles.push(returnCycle)
+
+      continue
+    }
+
+    missingReturns[returnRequirementId] = {
+      licence: {
+        endDate: missingReturnLog.licence_end_date ? new Date(missingReturnLog.licence_end_date) : null,
+        id: missingReturnLog.licence_id,
+        licenceRef: missingReturnLog.licence_ref,
+      },
+      returnVersion: {
+        endDate: missingReturnLog.return_version_end_date ? new Date(missingReturnLog.return_version_end_date) : null,
+        id: missingReturnLog.return_version_id,
+        startDate: new Date(missingReturnLog.return_version_start_date)
+      },
+      abstractionPeriod: {
+        startDay: missingReturnLog.abstraction_period_start_day,
+        startMonth: missingReturnLog.abstraction_period_start_month,
+        endDay: missingReturnLog.abstraction_period_end_day,
+        endMonth: missingReturnLog.abstraction_period_end_month
+      },
+      areaCode: missingReturnLog.area_code,
+      multipleUpload: missingReturnLog.water_undertaker,
+      reference: missingReturnLog.legacy_id,
+      reportingFrequency: missingReturnLog.reporting_frequency,
+      returnRequirementId: returnRequirementId,
+      siteDescription: missingReturnLog.site_description,
+      summer: missingReturnLog.summer,
+      twoPartTariff: missingReturnLog.two_part_tariff,
+      regionId: missingReturnLog.region_id,
+      returnCycles: [returnCycle]
+    }
+  }
+
+  return missingReturns
+}
+
+module.exports = {
+  go
+}
