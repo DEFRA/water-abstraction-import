a1498fc754fb3848469693e03234a2b41401e03b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-06-10 18:44:22 +0100

    Add query to fetch NALD return lines
    Our main query will provide the return reference, and region ID. We can use that to find all NALD returns lines that have a quantity, even if its 0.
    
    Then as we process 'due' return logs provided by our query, we can assign the lines to them.

---
+++ src/modules/missing-return-submissions/lib/fetch-nald-return-lines.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const db = require('../../../lib/connectors/db.js')
+const { compareDates } = require('../../../lib/date-helpers.js')
+
+async function go (regionId, returnReference) {
+  const params = [regionId, returnReference]
+
+  const query = `WITH old_return_lines AS (
+  SELECT
+    to_date(nrl."RET_DATE", 'DD/MM/YYYY') AS return_date,
+    nrl."RET_QTY" AS return_qty
+  FROM
+    public."NALD_RET_LINES" nrl
+  WHERE
+    nrl."FGAC_REGION_CODE" = $1
+    AND nrl."ARFL_ARTY_ID" = $2
+    AND nrl."RET_QTY" IS NOT NULL
+    AND nrl."RET_QTY" <> ''
+    AND nrl."RET_QTY" <> 'null'
+),
+extract_return_lines AS (
+  SELECT
+    to_date(nrl."RET_DATE", 'YYYYMMDDHH24MISS') AS return_date,
+    nrl."RET_QTY" AS return_qty
+  FROM
+    "import"."NALD_RET_LINES" nrl
+  WHERE
+    nrl."FGAC_REGION_CODE" = $1
+    AND nrl."ARFL_ARTY_ID" = $2
+    AND nrl."RET_QTY" IS NOT NULL
+    AND nrl."RET_QTY" <> ''
+    AND nrl."RET_QTY" <> 'null'
+),
+all_lines AS (
+  SELECT
+    orl.*
+  FROM
+    old_return_lines orl
+  UNION ALL
+  SELECT
+    erl.*
+  FROM
+    extract_return_lines erl
+)
+SELECT
+  al.return_date,
+  al.return_qty
+FROM
+  all_lines al
+ORDER BY
+  al.return_date ASC;`
+
+  const rows = await db.query(query, params)
+
+  return rows.map((row) => {
+    return {
+      returnDate: new Date(row.return_date),
+      qty: Number(row.return_qty)
+    }
+  })
+}
+
+module.exports = {
+  go
+}
