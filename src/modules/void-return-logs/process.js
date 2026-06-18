a4762bb27ba6722f7d4296ba2ee2122ebaf1771a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-06-18 20:57:12 +0100

    Add the query to void the return logs

--- src/modules/void-return-logs/process.js
+++ src/modules/void-return-logs/process.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const VoidWhereEndAfterLicenceEnds = require('./lib/void-where-end-after-licence-ends.js')
+const VoidWhereReturnRequirementDeleted = require('./lib/void-where-return-requirement-deleted.js')
 const { currentTimeInNanoseconds, calculateAndLogTimeTaken } = require('../../lib/general.js')
 
 async function go (log = false) {
@@ -10,6 +11,7 @@ async function go (log = false) {
     const startTime = currentTimeInNanoseconds()
 
     await VoidWhereEndAfterLicenceEnds.go()
+    await VoidWhereReturnRequirementDeleted.go()
 
     if (log) {
       calculateAndLogTimeTaken(startTime, 'void-return-logs: complete', { messages })
