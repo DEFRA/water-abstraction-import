ba751e3db5207f7aaf888ee7ec3e6ab7987e51d8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-07-15 08:54:57 +0100

    Add round quantities to step
    A number of the mismatches are caused by the initial import not handling JavaScript's floating point issues correctly. So the next step can more clearly identify mismatches caused by the dupe or week-start issues, we run this first.

--- src/modules/sync-nald-lines/process.js
+++ src/modules/sync-nald-lines/process.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const RoundQuantities = require('./lib/round-quantities.js')
 const { currentTimeInNanoseconds, calculateAndLogTimeTaken } = require('../../lib/general.js')
 
 async function go (log = false) {
@@ -7,6 +8,10 @@ async function go (log = false) {
 
   try {
     const startTime = currentTimeInNanoseconds()
+
+    // Rounding quantities to 6 decimal places fixes those previously imported that fell foul of JavaScript's issues
+    // with floating point numbers. This will avoid them being flagged as a mismatch in the next step.
+    await RoundQuantities.go()
 
     if (log) {
       calculateAndLogTimeTaken(startTime, 'sync-nald-lines: complete')
