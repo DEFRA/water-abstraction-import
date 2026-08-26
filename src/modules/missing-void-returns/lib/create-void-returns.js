08bc6ac56c08a50c5cc2ea70777e4ccc14146210
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-08-26 18:21:02 +0100

    Setup process to run region by region

---
+++ src/modules/missing-void-returns/lib/create-void-returns.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const db = require('../../../lib/connectors/db.js')
+
+async function go (regionCode) {
+  const params = [regionCode]
+}
+
+module.exports = {
+  go
+}
--- src/modules/missing-void-returns/process.js
+++ src/modules/missing-void-returns/process.js
@@ -1,11 +1,14 @@
 'use strict'
 
 const AlreadyRun = require('./lib/already-run.js')
+const CreateVoidReturns = require('./lib/create-void-returns.js')
 const RecordRun = require('./lib/record-run.js')
 const { currentTimeInNanoseconds, calculateAndLogTimeTaken } = require('../../lib/general.js')
 
 async function go (log = false) {
   const messages = []
+
+  let currentRegion
 
   try {
     const startTime = currentTimeInNanoseconds()
@@ -19,13 +22,21 @@ async function go (log = false) {
       return messages
     }
 
+    // Add the missing void return logs one region at a time
+    const regions = ['1', '2', '3', '4', '5', '6', '7', '8']
+
+    for (const region of regions) {
+      currentRegion = region
+      await CreateVoidReturns.go(region)
+    }
+
     await RecordRun.go()
 
     if (log) {
       calculateAndLogTimeTaken(startTime, 'missing-void-returns: complete', { messages })
     }
   } catch (error) {
-    global.GlobalNotifier.omfg('missing-void-returns: errored', {}, error)
+    global.GlobalNotifier.omfg('missing-void-returns: errored', { currentRegion }, error)
 
     messages.push(error.message)
   }
