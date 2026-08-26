a8236cd0dad5cc22939e58d25280177956dff1a5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-08-26 18:01:14 +0100

    Add step recording
    We've used in some of the other one-off steps to avoid running a step daily, when we only need it to run once.

--- src/modules/missing-void-returns/process.js
+++ src/modules/missing-void-returns/process.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const AlreadyRun = require('./lib/already-run.js')
+const RecordRun = require('./lib/record-run.js')
 const { currentTimeInNanoseconds, calculateAndLogTimeTaken } = require('../../lib/general.js')
 
 async function go (log = false) {
@@ -7,6 +9,17 @@ async function go (log = false) {
 
   try {
     const startTime = currentTimeInNanoseconds()
+
+    const hasAlreadyRun = await AlreadyRun.go()
+
+    if (hasAlreadyRun) {
+      global.GlobalNotifier.omg('missing-void-returns: skipped')
+      messages.push('Skipped because they have already been synced')
+
+      return messages
+    }
+
+    await RecordRun.go()
 
     if (log) {
       calculateAndLogTimeTaken(startTime, 'missing-void-returns: complete', { messages })
