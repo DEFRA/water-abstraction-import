c10c8a75a2da732ba97efda2896d180584cb214d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-09-01 14:45:37 +0100

    Wrap fetching and creating data in a module
    There's a lot going on already in `process.js`. So, we move the actual process of fetching and creating the missing void return logs into a separate module.

--- src/modules/missing-void-returns/lib/create-void-returns.js
+++ src/modules/missing-void-returns/lib/create-void-returns.js
@@ -1,9 +1,20 @@
 'use strict'
 
-const db = require('../../../lib/connectors/db.js')
+const CreateReturnLines = require('./create-return-lines.js')
+const CreateReturnLog = require('./create-return-log.js')
+const CreateReturnSubmission = require('./create-return-submission.js')
+const FetchMissingVoidReturns = require('./fetch-missing-void-returns.js')
 
-async function go (regionCode) {
-  const params = [regionCode]
+async function go (regionCode, timestamp) {
+  const missingVoidReturns = await FetchMissingVoidReturns.go(regionCode)
+
+  for (const missingVoidReturn of missingVoidReturns) {
+    const returnLog = await CreateReturnLog.go(missingVoidReturn, timestamp)
+
+    const returnSubmissionId = await CreateReturnSubmission.go(returnLog, timestamp)
+
+    await CreateReturnLines.go(missingVoidReturn, returnSubmissionId, timestamp)
+  }
 }
 
 module.exports = {
